b086bf28d4d42dad348ae08ff8bf3a49bd97978d
justinelaroza <larozajustine15@gmail.com>
2024-10-02 21:11:01 +0800

    Created a separate file for html forms for cleaner look, created the initial form of code verification

---
+++ login-form.php
@@ -0,0 +1,129 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login Page</title>
+    <link rel="stylesheet" href="login.css">
+</head>
+<body>
+    <div class="middle-section">
+        <div class="wrapper">
+            <form action="<?php echo htmlspecialchars($_SERVER['PHP_SELF']); ?>" method="post">
+                <div class="top-area">
+                    <h1>WELCOME</h1>
+                </div>
+                <div class="input-box">
+                    <label>Username:</label>
+                    <input type="text" name="username" placeholder="Username">
+                    <img src="./pictures/user-picture.png" alt="user-picture">
+                </div>
+                <div class="input-box">
+                    <label>Password:</label>
+                    <input type="password" name="password" placeholder="Password">
+                    <img src="./pictures/pass-picture.png" alt="pass-picture">
+                </div>
+                <div class="error-message">
+                    <?php
+                        if (isset($_GET['invalid'])) {
+                            echo $_GET['invalid']; // mag pprint to ng error message from the error handling sa php sa baba
+                        }
+                        if (isset($_GET['fill'])) {
+                            echo $_GET['fill'];
+                        }
+                    ?>
+                </div>
+                <div class="forgot">
+                    <input type="submit" name="forgot-button" value="Forgot Password?">
+                    <!--username check nya sa data base kung meron nga tas yung old pass check din kung tatama yung pass
+                    nasa data base tas yung new pass dapat mag UPDATE SET yung database bale-->
+                </div>
+                <button name="submit">Login</button>
+                <div class="register">
+                    <p>Don't have an account? <input type="submit" name="register-button" value="Register here"> </p>  
+                </div>
+            </form>
+        </div>
+        <div class="wrapper-right" <?php if(isset($_SESSION['show'])) { echo $_SESSION['show']; unset($_SESSION['show']); }?>>
+
+            <form action="<?php echo htmlspecialchars($_SERVER['PHP_SELF']); ?>" method="post">
+                <div class="register">
+                    <h1>REGISTER</h1>
+                </div>
+                <div class="full-name">
+                    <div class="name-field">
+                        <label>First Name:</label>
+                        <input type="text" name="firstname" placeholder="First Name">
+                    </div>
+                    <div class="name-field">
+                        <label>Last Name:</label>
+                        <input type="text" name="lastname" placeholder="Last Name">
+                    </div>
+                    </div>
+                <div class="other-input">
+                    <label>Address:</label>
+                    <input type="text" name="address" placeholder="Address">
+                </div>
+                <div class="parent">
+                    <div class="child">
+                        <label>Email Address:</label>
+                        <input type="email" name="email" placeholder="Email">
+                    </div>
+                    <div class="child">
+                        <label>Username:</label>
+                        <input type="text" name="user-register" placeholder="Username">
+                    </div>
+                </div>
+                <div class="parent">
+                    <div class="child">
+                        <label>Password:</label>
+                        <input type="password" name="orig-pass" placeholder="Password">
+                    </div>
+                    <div class="child">
+                        <label>Confirm Password:</label>
+                        <input type="password" name="confirm-pass" placeholder="Password">
+                    </div>
+                </div>
+                <div class="error-message-reg">
+                    <?php 
+                        if (isset($_SESSION['fillReg'])) {
+                            echo $_SESSION['fillReg'];
+                            unset($_SESSION['fillReg']);
+                        }
+                        if (isset($_SESSION['passMatch'])) {
+                            echo $_SESSION['passMatch'];
+                            unset($_SESSION['passMatch']);
+                        }
+                        if(isset($_SESSION['usedEmail'])) {
+                            echo $_SESSION['usedEmail'];
+                            unset($_SESSION['usedEmail']);
+                        }
+                        if(isset($_SESSION['usedUser'])) {
+                            echo $_SESSION['usedUser'];
+                            unset($_SESSION['usedUser']);
+                        }
+                    ?>
+                </div>
+                <button name="new-register" <?php if(isset($_SESSION['hideReg'])) { echo $_SESSION['hideReg']; unset($_SESSION['hideReg']); }?>>Register</button>
+                <div class="code-container" <?php if(isset($_SESSION['revealReg'])) { echo $_SESSION['revealReg']; unset($_SESSION['revealReg']); }?>>
+                    <div class="code-label">
+                        <label>Code sent to: Email@gmail.com</label>
+                    </div>
+                    <div class="code-input">
+                        <input type="text" maxlength="1" name="firstNum">
+                        <input type="text" maxlength="1" name="secondNum">
+                        <input type="text" maxlength="1" name="thirdNum">
+                        <input type="text" maxlength="1" name="fourthNum">
+                        <input type="text" maxlength="1" name="fifthNum">
+                        <input type="text" maxlength="1" name="sixthNum">
+                    </div>
+                    <div class="submit-code-parent">
+                        <input type="submit" name="codeSub" class="submit-code-button">
+                    </div>
+                </div>
+            </form>
+        </div>
+    </div>
+    
+</body>
+</html>
--- login.php
+++ login.php
@@ -2,139 +2,6 @@
     include('db.php');
     /** @var mysqli $connection */ // This tells the editor that $connection is a mysqli object.
     session_start();
-?>
-
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Login Page</title>
-    <link rel="stylesheet" href="login.css">
-</head>
-<body>
-    <div class="middle-section">
-        <div class="wrapper">
-            <form action="<?php echo htmlspecialchars($_SERVER['PHP_SELF']) ?>" method="post">
-                <div class="top-area">
-                    <h1>WELCOME</h1>
-                </div>
-                <div class="input-box">
-                    <label>Username:</label>
-                    <input type="text" name="username" placeholder="Username">
-                    <img src="./pictures/user-picture.png" alt="user-picture">
-                </div>
-                <div class="input-box">
-                    <label>Password:</label>
-                    <input type="password" name="password" placeholder="Password">
-                    <img src="./pictures/pass-picture.png" alt="pass-picture">
-                </div>
-                <div class="error-message">
-                    <?php
-                        if (isset($_GET['invalid'])) {
-                            echo $_GET['invalid']; // mag pprint to ng error message from the error handling sa php sa baba
-                        }
-                        if (isset($_GET['fill'])) {
-                            echo $_GET['fill'];
-                        }
-                    ?>
-                </div>
-                <div class="forgot">
-                    <input type="submit" name="forgot-button" value="Forgot Password?">
-                    <!--username check nya sa data base kung meron nga tas yung old pass check din kung tatama yung pass
-                    nasa data base tas yung new pass dapat mag UPDATE SET yung database bale-->
-                </div>
-                <button name="submit">Login</button>
-                <div class="register">
-                    <p>Don't have an account? <input type="submit" name="register-button" value="Register here"> </p>  
-                </div>
-            </form>
-        </div>
-        <div class="wrapper-right" <?php echo $_SESSION['show'];?>> <!-- if(isset($_SESSION['show'])) { echo $_SESSION['show']; unset($_SESSION['show']); } -->
-
-            <form action="<?php echo htmlspecialchars($_SERVER['PHP_SELF']) ?>" method="post">
-                <div class="register">
-                    <h1>REGISTER</h1>
-                </div>
-                <div class="full-name">
-                    <div class="name-field">
-                        <label>First Name:</label>
-                        <input type="text" name="firstname" placeholder="First Name">
-                    </div>
-                    <div class="name-field">
-                        <label>Last Name:</label>
-                        <input type="text" name="lastname" placeholder="Last Name">
-                    </div>
-                    </div>
-                <div class="other-input">
-                    <label>Address:</label>
-                    <input type="text" name="address" placeholder="Address">
-                </div>
-                <div class="parent">
-                    <div class="child">
-                        <label>Email Address:</label>
-                        <input type="email" name="email" placeholder="Email">
-                    </div>
-                    <div class="child">
-                        <label>Username:</label>
-                        <input type="text" name="user-register" placeholder="Username">
-                    </div>
-                </div>
-                <div class="parent">
-                    <div class="child">
-                        <label>Password:</label>
-                        <input type="password" name="orig-pass" placeholder="Password">
-                    </div>
-                    <div class="child">
-                        <label>Confirm Password:</label>
-                        <input type="password" name="confirm-pass" placeholder="Password">
-                    </div>
-                </div>
-                <div class="error-message-reg">
-                    <?php 
-                        if (isset($_SESSION['fillReg'])) {
-                            echo $_SESSION['fillReg'];
-                            unset($_SESSION['fillReg']);
-                        }
-                        if (isset($_SESSION['passMatch'])) {
-                            echo $_SESSION['passMatch'];
-                            unset($_SESSION['passMatch']);
-                        }
-                        if(isset($_SESSION['usedEmail'])) {
-                            echo $_SESSION['usedEmail'];
-                            unset($_SESSION['usedEmail']);
-                        }
-                        if(isset($_SESSION['usedUser'])) {
-                            echo $_SESSION['usedUser'];
-                            unset($_SESSION['usedUser']);
-                        }
-                    ?>
-                </div>
-                <button name="new-register">Register</button>
-                <div class="code-container">
-                    <div class="code-label">
-                        <label>Code sent to: Email@gmail.com</label>
-                    </div>
-                    <div class="code-input">
-                        <input type="text" maxlength="1">
-                        <input type="text" maxlength="1">
-                        <input type="text" maxlength="1">
-                        <input type="text" maxlength="1">
-                        <input type="text" maxlength="1">
-                        <input type="text" maxlength="1">
-                    </div>
-                    <div class="submit-code-parent">
-                        <input type="submit" name="code-submit" value="Submit" class="submit-code-button">
-                    </div>
-                </div>
-            </form>
-        </div>
-    </div>
-    
-</body>
-</html>
-
-<?php
 
     function styleReveal() {
         $_SESSION['show'] = "style='display: block !important'";
@@ -234,7 +101,47 @@
             //registering the user input to the database
             else {
                 
+                $_SESSION['revealReg'] = "style='display: block !important'";
+                $_SESSION['hideReg'] = "style='display: none !important'";
+                styleReveal();
 
+                $randomNum = [];
+                    for ($i = 0; $i < 6; $i++) {
+                        $randomNum[$i] = rand(0, 9); 
+                    }
+
+                foreach ($randomNum as $num) {
+                    echo $num;
+                }
+
+                if (isset($_POST['codeSub'])) {
+                    echo 'pindot';
+                    if (isset($_POST['firstNum'], $_POST['secondNum'], $_POST['thirdNum'], $_POST['fourthNum'], $_POST['fifthNum'], $_POST['sixthNum']) ) {
+
+                        if ($_POST['firstNum'] == $randomNum[0] && $_POST['secondNum'] == $randomNum[1] && $_POST['thirdNum'] == $randomNum[2] && $_POST['fourthNum'] == $randomNum[3] && $_POST['fifthNum'] == $randomNum[4] && $_POST['sixthNum'] == $randomNum[5]) { 
+                            echo "Code is correct!";
+                            unset($_SESSION['randomNum']);
+                        } else {
+                            echo "Code is incorrect!";
+                        }
+                    }
+                    else {
+                        echo 'set codes';
+                        //$_SESSION['codeError'] = 'Please enter the verification code!';
+                    }
+                } 
+
+                /*
+                if (!isset($_SESSION['randomNum'])) { //if wala pang random code na nakuha si user para di paulit ulit na mag random code pag nag refresh
+                    $randomNum = [];
+                    for ($i = 0; $i < 6; $i++) {
+                        $randomNum[$i] = rand(0, 9); 
+                    }
+                    $_SESSION['randomNum'] = $randomNum;
+                } else {
+                    $randomNum = $_SESSION['randomNum']; //if nakakuha na code si user tas ni refresh for some reason yung prev code padin makukuha
+                }
+                
                 $hashPass = password_hash($origPass, PASSWORD_DEFAULT); //hash the password before going to db
 
                 $slqInsertReg = "INSERT INTO register_data(first_name, last_name, address, email, username, password) VALUES ('$firstname', '$lastname', '$address', '$email', '$userRegister', '$hashPass')";  
@@ -242,6 +149,7 @@
 
                 mysqli_query($connection, $slqInsertReg); //irurun ang query
                 mysqli_query($connection, $sqlInsertLog ); 
+                */
 
             }
         }
@@ -272,6 +180,6 @@
     }
 */
 
-
-    mysqli_close($connection);
+include('login-form.php'); // Include the HTML form after processing the logic kase nag eerror ng Cannot modify header information - headers already sent need muna manuna lgic kesa html
+mysqli_close($connection);
 ?>
